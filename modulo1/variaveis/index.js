61e2d0ee92951c6f7f371e910d6b2214c3fa2ed2
karolina-azevedo <karolinam.azevedo@yahoo.com.br>
2022-04-12 16:21:15 -0300

    respostas-dos-exercicios-de-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,48 @@
+// exercícios de interpretação
+// questao 1
+//10
+//10 5
+
+//questao 2
+// 10 10 10
+
+//questao 3
+//p = horasDeTrabalho
+//t = valorEmReais
+
+// exercícios de escrita de código
+
+// questão 1
+
+// foram impressos como undefined por nao ter valores atribuídos
+
+// mudou o tipo para string
+
+//let nome = prompt ("Qual é o seu nome?")
+//let idade = prompt ("Qual é a sua idade?")
+//console.log("Olá", nome, "você tem", idade, "anos")
+
+//questão 2
+
+//prompt ("Você assistiu TV hoje?")
+//prompt ("Você usou computador hoje?")
+//prompt ("Você ouviu música hoje?")
+
+//let tv = prompt ("Você assistiu TV hoje?")
+//let computador = prompt ("Você usou computador hoje?")
+//let musica = prompt ("Você ouviu música hoje?")
+
+//console.log("Você assistiu TV hoje?", tv)
+//console.log("Você usou computador hoje?", computador)
+//console.log("Você ouviu música hoje?", musica)
+
+//questão 3
+
+//let a = 10
+//let b = 25
+//let c = 10
+//a = b
+//b = c
+//console.log("O novo valor de a é", a)
+//console.log("O novo valor de b é", b)
+
