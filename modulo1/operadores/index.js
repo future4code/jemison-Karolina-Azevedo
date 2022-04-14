e73df5bc1af0893953aa8a445b499868de6f7672
karolina-azevedo <karolinam.azevedo@yahoo.com.br>
2022-04-14 14:20:04 -0300

    respostas-de-exercicios-operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,69 @@
+/*Exercícios de interpretação de código
+
+1- a. false    b. false   c. true   d. booleans
+
+2- O valor do número é uma string, não podendo ser somado.
+Aparecerá no console os 2 números escritos pelo usuário
+
+3-
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+primeiroNumero = Number (primeiroNumero)
+segundoNumero = Number (segundoNumero)
+const soma = primeiroNumero + segundoNumero
+console.log(soma)
+
+//Exercícios de escrita de código
+
+1.
+
+let suaIdade = prompt ("Qual a sua idade?")
+let idadeAmigo = prompt ("Qual a idade do seu melhor amigo?")
+
+suaIdade = Number (suaIdade)
+idadeAmigo = Number (idadeAmigo)
+
+resultado = suaIdade > idadeAmigo
+console.log("Sua idade é maior do que a do seu melhor amigo?", resultado)
+
+resultado = idadeAmigo - suaIdade
+console.log(resultado)
+
+2.
+
+let numeroPar = prompt ("Insira um número par")
+numeroPar = Number (numeroPar)
+resultado = numeroPar % 2
+console.log("O resto da divisão é", resultado)
+
+3.
+
+idadeEmAnos = prompt ("Quantos anos você tem?")
+idadeEmAnos = Number (idadeEmAnos)
+
+idadeEmMeses = idadeEmAnos * 12
+console.log("a.", idadeEmMeses, "meses")
+
+idadeEmDias = idadeEmAnos * 365
+console.log("b.", idadeEmDias, "dias")
+
+idadeEmHoras = idadeEmAnos * 365 * 24
+console.log("c.", idadeEmHoras, "horas")
+
+4. 
+
+let numero1 = prompt("Qual o primeiro número?")
+let numero2 = prompt("Qual o segundo número?")
+
+numero1 = Number (numero1)
+numero2 = Number (numero2)
+
+resultado1 = numero1 > numero2
+resultado2 = numero1 === numero2
+resultado3 = numero1 % numero2
+resultado4 = numero2 % numero1
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)*/
