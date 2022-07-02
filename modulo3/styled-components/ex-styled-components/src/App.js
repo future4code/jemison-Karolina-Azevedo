d570dce471db3dac3fca6adcfa29c8e786e59f16
karolina-azevedo <karolinam.azevedo@yahoo.com.br>
2022-07-02 15:27:02 -0300

    exercício-styled-components-atualizadp

--- modulo3/styled-components/ex-styled-components/src/App.js
+++ modulo3/styled-components/ex-styled-components/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import logoLabenu from './img/logoLabenu.jpg';
+import {Container} from './styled';
+import {Header} from './styled';
+import {Main} from './styled';
+import {Footer} from './styled';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <Header>
+        <img src={ logoLabenu } alt={"logo Labenu"} />
+        <h1>LabZap</h1>
+      </Header>
+
+      <Main>
+        <section></section>
+
+        <div>
+         Remetente: <input  type="text" name="input"/>
+         Msg: <input className='inputModificado' type="text" name="input" />
+        <button type="button">Enviar Mensagem</button>
+        </div>
+
+        <section></section>
+      </Main>
+
+      <Footer>
+        <p>Copyright  © 2022 Labenu All rigths reserved. R. Pais leme, 215, Conjunto 820 Pinheiros. Cep 05424-150</p>
+      </Footer>
+    </Container>
+
   );
 }
 
