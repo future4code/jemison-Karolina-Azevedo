7dfb9170a6baa164d045ef932fea0f5339feb359
karolina-azevedo <karolinam.azevedo@yahoo.com.br>
2022-08-02 19:34:44 -0300

    estrutura-projeto

---
+++ modulo3/projeto-astromatch/projeto-astromatch/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { GlobalStyle } from './GlobalStyle';
+import {TelaInicial} from './Telas/TelaInicial'
+
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle/>
+     <TelaInicial/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo3/projeto-astromatch/projeto-astromatch/src/GlobalStyle.js
@@ -0,0 +1,33 @@
+
+import {createGlobalStyle} from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+:root {
+    --soft-white: #f2f2f2;
+    --green: #8bcf21;
+    --yelow: #ffcb16;
+    --dark: #383b40;
+    --deep-blue: #011640;
+    --deep-blue-secondary: #010F2B
+
+}
+
+h1, h2, h3 {
+    font-family: 'Open Sans', cursive;
+    color: blue
+}
+
+p{
+    font-family: 'Merriweather', cursive;
+}
+
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #ffcb16
+}
+
+*{
+    margin: 0;
+    padding: 0;
+}
+`
