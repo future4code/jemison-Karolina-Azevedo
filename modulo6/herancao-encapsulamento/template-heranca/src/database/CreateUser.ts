24d130dd3a48dfdbb71a2163b720537d82d043de
karolina-azevedo <karolinam.azevedo@yahoo.com.br>
2022-12-08 19:08:42 -0300

    exercício-heranca-encapsulamento

---
+++ modulo6/herancao-encapsulamento/template-heranca/src/database/CreateUser.ts
@@ -0,0 +1,11 @@
+// import { BaseDatabase } from "./BaseDatabase"
+
+// export class UserDatabase extends BaseDatabase {
+// public static TABLE_USERS = "Labe_Users"
+// public async createUser() {
+// const result = await BaseDatabase
+// .connection(UserDatabase.TABLE_USERS)
+// .insert(?)
+// return result
+// }
+// }
